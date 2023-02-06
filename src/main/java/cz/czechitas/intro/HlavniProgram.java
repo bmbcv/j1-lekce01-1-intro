32d0f53d955fd22e9020e09bb2cd46db2deb3756
Vlastimil Cernicek <vlastimil.cernicek@email.cz>
2023-02-06 23:17:39 +0100

    Hotovo - rybka obepluje kamen

--- src/main/java/cz/czechitas/intro/HlavniProgram.java
+++ src/main/java/cz/czechitas/intro/HlavniProgram.java
@@ -1,14 +1,22 @@
 package cz.czechitas.intro;
 
-import cz.czechitas.intro.api.*;
-import net.sevecek.util.*;
+import cz.czechitas.intro.api.Fish;
+import cz.czechitas.intro.api.Stone;
 
 public class HlavniProgram {
 
     public void main(String[] args) {
 
-        new Fish(50, 50);
-        // TODO: Sem vepiste svuj program
+
+        Fish fish = new Fish(50, 50);
+
+        new Stone(150, 150);
+
+        for (int i = 0; i < 4; i++) {
+            fish.moveForward(200);
+            fish.turnRight();
+        }
+
 
     }
 
